3740f8e9f5b5099c8e2ebdfbae056c5798342426
Petrik Diak <diak@petrik.hu>
2023-12-08 12:42:31 +0100

    Update Delete

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Render, Res} from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Render, Res} from '@nestjs/common';
 import * as mysql from 'mysql2';
 import { AppService } from './app.service';
 import { SongDTO } from './Song';
@@ -19,7 +19,7 @@ export class AppController {
   @Get()
   @Render('index')
   async index() {
-    const [songs] = await conn.execute("SELECT title,artist,length FROM songs ORDER BY artist, title");
+    const [songs] = await conn.execute("SELECT title,artist,length,id FROM songs ORDER BY artist, title");
     return {songs};
   }
 
@@ -56,6 +56,54 @@ export class AppController {
     }
     else{
       const [result] = await conn.execute('INSERT INTO songs (title,artist,length) VALUES (?,?,?)',[title,artist,length]); 
+      console.log(result);
+      response.redirect('/');
+    }
+    return {};
+  }
+
+  @Post("/deleteSong/:id")
+  async deleteSong(@Param('id') id: number, @Res() res:Response){
+    const [result] = await conn.execute('DELETE FROM songs WHERE id=?',[id]);
+    console.log(result);
+    res.redirect('/');
+  }
+  
+  @Get("/modSong/:id")
+  @Render("modSong")
+  async modForm(@Param('id') id:number){
+    const [songs] = (await conn.execute('SELECT title,artist,length FROM songs WHERE id=?',[id]));
+    console.log(songs);
+    return {title: songs[0].title, artist: songs[0].artist, length: songs[0].length, error:''}    
+  }
+
+  @Post("/modSong/:id")
+  @Render("modSong")
+  async modSong(@Param('id') id:number ,@Body() song: SongDTO, @Res() response: Response){
+    const title = song.title;
+    const artist = song.artist;
+    const length = song.length;
+
+    let error = '';
+    
+    if(title.trim().length < 1){
+      error = 'All fields are required!';
+      return {error ,title:'', artist, length};
+    }
+    if(artist.trim().length < 1){
+      error = 'All fields are required!';
+      return {error, title, artist:'', length};
+    }
+    if(!length){
+      error = 'All fields are required!';
+      return {error, artist, title, length};
+    }
+    if(length < 3){
+      error = 'The length must be longer than 3sec!'
+      return {error, title, artist, length}
+    }
+    else{
+      const [result] = await conn.execute('UPDATE songs SET title = ?,artist = ?,length = ? WHERE id=?',[title,artist,length,id]); 
       console.log(result);
       response.redirect('/');
     }
